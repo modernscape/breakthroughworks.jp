057c1ba3c046f3f1e9f090cd4fb60701aebae57f
modernscape <modernscape@gmail.com>
2026-01-08 22:13:12 +0900

    26-01-08_

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import {useEffect, useRef} from "react";
 import {useGLTF} from "@react-three/drei";
+import {useFrame} from "@react-three/fiber";
+import * as THREE from "three";
+import loadCenters from "@/lib/loadCenters";
+import applyMeshAnimation from "@/lib/applyMeshAnimation";
 
 export default function Model() {
   const {scene} = useGLTF("/model/24-11-10_sphere.glb");
+
   return <primitive object={scene} />;
 }
---
+++ src/lib/applyMeshAnimation.ts
@@ -0,0 +1,3 @@
+export default function applyMeshAnimation() {
+  return null;
+}
