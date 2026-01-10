c4f7b91f16333829f559532cea7ecfa11fca56fe
modernscape <modernscape@gmail.com>
2026-01-10 22:36:29 +0900

    26-01-10_

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,14 +1,42 @@
-"use client";
+"use client"
 
-import {useEffect, useRef} from "react";
-import {useGLTF} from "@react-three/drei";
-import {useFrame} from "@react-three/fiber";
-import * as THREE from "three";
-import loadCenters from "@/lib/loadCenters";
-import applyMeshAnimation from "@/lib/applyMeshAnimation";
+import {useEffect, useRef} from "react"
+import {useGLTF} from "@react-three/drei"
+import {useFrame} from "@react-three/fiber"
+import * as THREE from "three"
+import {loadCenters} from "@/lib/loadCenters"
+import {CenterData} from "@/types/center"
+import {applyMeshAnimation} from "@/lib/applyMeshAnimation"
 
 export default function Model() {
-  const {scene} = useGLTF("/model/24-11-10_sphere.glb");
+  const {scene} = useGLTF("/model/24-11-10_sphere.glb")
+  const centersRef = useRef<Map<string, CenterData>>(new Map())
 
-  return <primitive object={scene} />;
+  useEffect(() => {
+    loadCenters().then((map) => {
+      centersRef.current = map
+      applyMeshAnimation(scene, centersRef.current)
+    })
+  }, [scene])
+
+  useFrame(() => {
+    scene.traverse((obj) => {
+      const mesh = obj as THREE.Mesh
+      const anim = mesh.userData.anim
+      if (!anim) return
+
+      if (anim.step < 0) {
+        anim.forward = true
+        anim.step = 0 // MIN:0
+      } else if (anim.step > 1) {
+        anim.forward = false
+        anim.step = 1 // MAX:1
+      }
+
+      anim.step += anim.forward ? anim.speed : -anim.speed
+      mesh.position.copy(anim.direction.clone().multiplyScalar(anim.step)) // mix
+    })
+  })
+
+  return <primitive object={scene} />
 }
--- src/lib/applyMeshAnimation.ts
+++ src/lib/applyMeshAnimation.ts
@@ -1,3 +1,25 @@
-export default function applyMeshAnimation() {
-  return null;
+import {CenterData} from "@/types/center"
+import * as THREE from "three"
+
+export function applyMeshAnimation(
+  scene: THREE.Object3D, //
+  centers: Map<string, CenterData>
+) {
+  scene.traverse((obj) => {
+    if (!(obj as THREE.Mesh).isMesh) return
+
+    const mesh = obj as THREE.Mesh
+    const center = centers.get(mesh.name) //CenterData
+    if (!center) return
+
+    const c0 = new THREE.Vector3(...center.p0)
+    const c1 = new THREE.Vector3(...center.p1)
+
+    mesh.userData.anim = {
+      direction: c0.clone().sub(c1),
+      step: center.step,
+      speed: center.speed,
+      forward: true,
+    }
+  })
 }
