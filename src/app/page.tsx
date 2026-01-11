8ee6ee6e92974012faf80a89fb20bbf5d4e6d026
modernscape <modernscape@gmail.com>
2026-01-12 00:27:36 +0900

    26-01-12_やり直す

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Scene from "@/components/Scene";
+import Scene from "@/components/Scene"
 
 export default function Page() {
   return (
     <main style={{width: "100vw", height: "100vh"}}>
       <Scene />
     </main>
-  );
+  )
 }
+
+// やり直す
