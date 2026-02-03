8c827ee1aa29bfb6a2f48b10d27633c5c43b945a
modernscape <modernscape@gmail.com>
2026-02-03 19:12:55 +0900

    projects フォルダ内のコンポーネントをビルド対象として認識させる

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-};
+  // projects フォルダ内のコンポーネントをビルド対象として認識させる
+  transpilePackages: ["projects/sphere", "projects/sphere_distortion"],
+}
 
-export default nextConfig;
+export default nextConfig
