82d53e417d06992daf282b4a49c6682deeef954b
modernscape <modernscape@gmail.com>
2026-02-03 18:54:46 +0900

    SpherePageのパス修正

--- src/app/sphere/page.tsx
+++ src/app/sphere/page.tsx
@@ -1,4 +1,6 @@
-import SpherePage from "../../../projects/sphere"
+// import SpherePage from "../../../projects/sphere"
+// import SphereProject from "../../../projects/sphere"
+import SpherePage from "@/../projects/sphere"
 
 export default function Page() {
   return <SpherePage />
--- src/app/sphere_distortion/page.tsx
+++ src/app/sphere_distortion/page.tsx
@@ -1,4 +1,5 @@
-import SpherePage from "../../../projects/sphere_distortion"
+// import SpherePage from "../../../projects/sphere_distortion"
+import SpherePage from "@/../projects/sphere_distortion"
 
 export default function Page() {
   return <SpherePage />
