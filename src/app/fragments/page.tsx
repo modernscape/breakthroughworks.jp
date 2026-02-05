13afadcba3d0dba7c0fb4e390a6f1d44fb39fb3b
modernscape <modernscape@gmail.com>
2026-02-05 22:24:32 +0900

    typo

--- src/app/fragments/page.tsx
+++ src/app/fragments/page.tsx
@@ -3,7 +3,7 @@
 export default function FragmentsEmbed() {
   return (
     <iframe
-      src="https://modernscape.github.io/Mid3_fragments/o"
+      src="https://modernscape.github.io/Mid3_fragments/"
       style={{
         width: "100vw",
         height: "100vh",
