6d1b09b300c1383d5ea66ccf82c8987c619558f8
modernscape <modernscape@gmail.com>
2026-02-06 14:42:00 +0900

    Mid3_追加

--- src/app/imgeToPoints/page.tsx
+++ src/app/imgeToPoints/page.tsx
@@ -3,7 +3,7 @@
 export default function FragmentsEmbed() {
   return (
     <iframe
-      src="https://modernscape.github.io/imageToPoints/"
+      src="https://modernscape.github.io/Mid3_imageToPoints/"
       style={{
         width: "100vw",
         height: "100vh",
